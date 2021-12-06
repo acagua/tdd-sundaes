043a1f7eb98af17c51a4b73a618f592dcba05d3d
Andres Cagua <andres.cagua@pluto.tv>
2021-12-06 14:54:46 -0500

    add mock server settings

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 
 export const SummaryForm = () => {
+  const [checked, setChecked] = useState(false);
+  const [showPopover, setShowPopover] = useState(false);
+
   const checkboxLabel = (
     <span>
       I agree to <span style={{ color: "blue" }}> Terms and Conditions </span>
     </span>
   );
 
-  const [checked, setChecked] = useState(false);
+  const popover = showPopover ? (
+    <div>No icecream will actually be Delivered</div>
+  ) : (
+    <></>
+  );
+
   return (
     <div>
       <input
@@ -17,9 +25,16 @@ export const SummaryForm = () => {
         type="checkbox"
       />
       <label htmlFor="tyc-checkbox">
-        I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+        I agree to{" "}
+        <span
+          style={{ color: "blue" }}
+          onMouseLeave={() => setShowPopover(false)}
+          onMouseEnter={() => setShowPopover(true)}
+        >
+          Terms and Conditions
+        </span>
       </label>
-
+      {popover}
       <button type="submit" disabled={!checked}>
         Confirm Order
       </button>
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,35 +1,53 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import { SummaryForm } from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
+describe("Validate order summary form behavior", () => {
+  test("Default behavior: checkbox is unchecked and button disabled", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /I agree to Terms and Conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+    expect(checkbox).not.toBeChecked();
+    expect(confirmButton).toBeDisabled();
+  });
 
-// describe("Validate order summary form behavior", () => {
-test("Default behavior: checkbox is unchecked and button disabled", () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole("checkbox", {
-    name: /I agree to Terms and Conditions/i,
+  test("summary form behavior when checked", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /I agree to Terms and Conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+
+    expect(checkbox).not.toBeChecked();
+    expect(confirmButton).toBeDisabled();
+    userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    expect(confirmButton).toBeEnabled();
+    userEvent.click(checkbox);
+    expect(checkbox).not.toBeChecked();
+    expect(confirmButton).toBeDisabled();
   });
-  const confirmButton = screen.getByRole("button", {
-    name: /confirm order/i,
+
+  test("popover responds to hover", () => {
+    render(<SummaryForm />);
+    const popoverString = /no icecream will actually be delivered/i;
+    const tycString = /terms and conditions/i;
+    const nullPopover = screen.queryByText(popoverString);
+    expect(nullPopover).not.toBeInTheDocument();
+
+    const tycInfo = screen.getByText(tycString);
+    userEvent.hover(tycInfo);
+    //its not query by so if not found = throws error but its more legible this way
+    const popover = screen.getByText(popoverString);
+    expect(popover).toBeInTheDocument(); //not needed, but more readable > best practices
+
+    userEvent.unhover(tycInfo);
+    const nullUnhoveredPopover = screen.queryByText(popoverString);
+    expect(nullUnhoveredPopover).not.toBeInTheDocument();
   });
-  expect(checkbox).not.toBeChecked();
-  expect(confirmButton).toBeDisabled();
 });
-
-test("summary form behavior when checked", () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole("checkbox", {
-    name: /I agree to Terms and Conditions/i,
-  });
-  const confirmButton = screen.getByRole("button", {
-    name: /confirm order/i,
-  });
-
-  expect(checkbox).not.toBeChecked();
-  expect(confirmButton).toBeDisabled();
-  fireEvent.click(checkbox);
-  expect(checkbox).toBeChecked();
-  expect(confirmButton).toBeEnabled();
-  fireEvent.click(checkbox);
-  expect(checkbox).not.toBeChecked();
-  expect(confirmButton).toBeDisabled();
-});
-// });
